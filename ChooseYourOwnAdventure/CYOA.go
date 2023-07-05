fc994faeef0df455a82fcfab198fd7a013870c3b
Prakhar Kumar <prakhar.meerut@gmail.com>
2023-07-05 17:35:52 +0530

    Parsing Json Stories file

--- ChooseYourOwnAdventure/CYOA.go
+++ ChooseYourOwnAdventure/CYOA.go
@@ -1,19 +1,49 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
+type StoryOptions struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+type Story struct {
+	Title   string         `json:"title"`
+	Story   []string       `json:"story"`
+	Options []StoryOptions `json:"options"`
+}
+
+func loadStoriesFile() map[string]Story {
+	result := make(map[string]Story)
+
+	data, err := os.ReadFile("gopher.json")
+	if err != nil {
+		log.Fatal("Error loading json file", err)
+	}
+
+	json.Unmarshal(data, &result)
+
+	return result
+}
+
 func main() {
+	loadStoriesFile()
+
 	mux := http.NewServeMux()
 
-	story := func(w http.ResponseWriter, r *http.Request) {
+	storiesHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r)
 		io.WriteString(w, "Hello")
 	}
 
-	mux.HandleFunc("/", story)
+	mux.HandleFunc("/", storiesHandler)
 
-	log.Fatal(http.ListenAndServe(":4000", mux))
+	// log.Fatal(http.ListenAndServe(":4000", mux))
+
 }
